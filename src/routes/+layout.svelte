f35692031396a227c0883d738d34d6fa6299b888
Julian Krzysiak <jkrzysiak13@gmail.com>
2024-04-01 00:51:02 -0700

    Setup working example of table component

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <div class="p-8 flex flex-col min-h-screen">
-	<header class="text-2xl">What to do AV</header>
+	<header class="text-2xl"><h1>What to do AV</h1></header>
 	<main class="flex-1 py-4">
 		<slot />
 	</main>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,10 @@
-<div>find what's up and coming</div>
+<script lang="ts">
+	import Datatable from '../lib/components/Datatable.svelte';
+</script>
+
+<div class="flex flex-col gap-2">
+	<h2 class="text-xl">find events to do near you</h2>
+	<h3>curated by Julian</h3>
+
+	<Datatable />
+</div>
