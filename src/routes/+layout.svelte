cdea207c1f63ba0e65c003baf898e19c0e543a75
Julian Krzysiak <jkrzysiak13@gmail.com>
2024-03-31 23:50:27 -0700

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import '../app.postcss';
+</script>
+
+<div class="p-8 flex flex-col min-h-screen">
+	<header class="text-2xl">What to do AV</header>
+	<main class="flex-1 py-4">
+		<slot />
+	</main>
+	<footer>Made by Julian Krzysiak</footer>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,1 @@
+<div>find what's up and coming</div>
