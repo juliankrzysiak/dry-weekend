7483c505be3f8ebd93d12806b30c899a87ed5bbc
Julian Krzysiak <jkrzysiak13@gmail.com>
2024-04-02 20:41:44 -0700

    Add Card when screen too small

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,8 +8,7 @@
 	<h3>curated by Julian</h3>
 </div>
 
-<div class="table-container">
-	<!-- Native Table Element -->
+<div class="table-container hidden md:block">
 	<table class="table table-hover">
 		<thead>
 			<tr>
@@ -32,4 +31,28 @@
 			{/each}
 		</tbody>
 	</table>
+</div>
+<div class="grid md:hidden gap-4">
+	{#each data.records as event}
+		<div class="card">
+			<header class="card-header flex justify-between">
+				<h2 class="text-xl">
+					<a href={event.url} class="underline">{event.title}</a>
+				</h2>
+				<p>${event.price}</p>
+			</header>
+			<section class="p-4">
+				<strong>
+					{format(event.date.slice(0, -1), 'EEEE, MMMM d · h:mm aaa')}
+				</strong>
+				in
+				<strong>
+					{event.location}
+				</strong>
+			</section>
+			<footer class="card-footer">
+				Organized by <strong>{event.organizer} </strong>
+			</footer>
+		</div>
+	{/each}
 </div>
