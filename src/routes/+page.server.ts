b276bde6f92432915df4ed75d7baf424efa198bd
Julian Krzysiak <jkrzysiak13@gmail.com>
2024-04-03 01:26:00 -0700

    Add filtering events past date
    I might add a separate section for past events, and keep only a weeks
    worth of events there.

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -4,7 +4,8 @@ const pb = new PocketBase('http://127.0.0.1:8090');
 
 export async function load() {
 	const records = await pb.collection('events').getFullList({
-		sort: '-created'
+		filter: 'date > @now',
+		sort: '+date'
 	});
 	return { records };
 }
