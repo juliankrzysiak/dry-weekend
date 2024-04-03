f679e3198b78312147fae666c3b4de616deb3d34
Julian Krzysiak <jkrzysiak13@gmail.com>
2024-04-02 22:55:21 -0700

    Update default style

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,23 +1,25 @@
-import { join } from 'path'
-import type { Config } from 'tailwindcss'
-import { skeleton } from '@skeletonlabs/tw-plugin'
+import { join } from 'path';
+import type { Config } from 'tailwindcss';
+import { skeleton } from '@skeletonlabs/tw-plugin';
 
 export default {
 	darkMode: 'class',
-	content: ['./src/**/*.{html,js,svelte,ts}', join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')],
+	content: [
+		'./src/**/*.{html,js,svelte,ts}',
+		join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')
+	],
 	theme: {
-		extend: {},
+		extend: {}
 	},
 	plugins: [
 		skeleton({
 			themes: {
 				preset: [
 					{
-						name: 'skeleton',
-						enhancements: true,
-					},
-				],
-			},
-		}),
-	],
+						name: 'vintage'
+					}
+				]
+			}
+		})
+	]
 } satisfies Config;
