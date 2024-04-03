153113a2093e67abe5c5d0c232cb1e71c0e2b563
Julian Krzysiak <jkrzysiak13@gmail.com>
2024-04-03 00:45:04 -0700

    Add toggling of dark mode

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,13 @@
 <script lang="ts">
 	import '../app.postcss';
+	import { LightSwitch } from '@skeletonlabs/skeleton';
 </script>
 
 <div class="p-8 flex flex-col min-h-screen">
-	<header class="text-2xl pb-8"><h1>desert weekend</h1></header>
+	<header class="flex justify-between text-2xl pb-8">
+		<h1>desert weekend</h1>
+		<LightSwitch />
+	</header>
 	<main class="flex-1 flex flex-col gap-8">
 		<slot />
 	</main>
