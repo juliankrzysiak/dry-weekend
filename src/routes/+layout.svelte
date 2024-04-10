e11f96eae501a542b48f178f6dce08300782c6b8
Julian Krzysiak <jkrzysiak13@gmail.com>
2024-04-10 00:30:25 -0700

    Update footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,13 +3,16 @@
 	import { LightSwitch } from '@skeletonlabs/skeleton';
 </script>
 
-<div class="p-8 flex flex-col min-h-screen">
+<div class="px-8 pt-8 pb-2 flex flex-col min-h-screen">
 	<header class="flex justify-between text-2xl pb-8">
-		<h1>desert weekend</h1>
+		<h1>dry weekend</h1>
 		<LightSwitch />
 	</header>
 	<main class="flex-1">
 		<slot />
 	</main>
-	<footer>Made by Julian Krzysiak</footer>
+	<footer class="flex justify-between">
+		<a href="https://juliankrzysiak.com/"> © 2024 Julian Krzysiak </a>
+		<a href="https://github.com/CastillejaCode/events">github</a>
+	</footer>
 </div>
