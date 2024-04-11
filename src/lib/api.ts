e8df29f69418ba0b9ae93fc147cc64b62399ed25
Julian Krzysiak <jkrzysiak13@gmail.com>
2024-04-11 12:24:49 -0700

    Delete example file

--- src/lib/api.ts
+++
@@ -1,8 +0,0 @@
-export default [
-	{ id: 1, z: 'Tobie', last_name: 'Vint', email: 'tvint0@fotki.com' },
-	{ id: 2, first_name: 'Zacharias', last_name: 'Cerman', email: 'zcerman1@sciencedirect.com' },
-	{ id: 3, first_name: 'Gérianna', last_name: 'Bunn', email: 'gbunn2@foxnews.com' },
-	{ id: 4, first_name: 'Bee', last_name: 'Saurin', email: 'bsaurin3@live.com' },
-	{ id: 5, first_name: 'Méyère', last_name: 'Granulette', email: 'mgranul4@yellowbook.com' }
-	// ...
-];
