082720de817dcf9eb7866ea5b9c305d3bc865f6c
Julian Krzysiak <jkrzysiak13@gmail.com>
2024-04-12 19:05:29 -0700

    Fix input forcing margin on paragraph

--- src/lib/components/RegionFilters.svelte
+++ src/lib/components/RegionFilters.svelte
@@ -3,13 +3,15 @@
 	const initialRegions = regions;
 </script>
 
-<div class="flex w-full max-w-sm justify-center gap-4">
+<fieldset class="flex w-full max-w-sm justify-center gap-4">
 	{#each initialRegions as region}
 		<label
 			class="variant-ghost-surface btn flex-1 has-[:checked]:variant-ghost-primary has-[:focus-visible]:outline"
 		>
-			<input class="absolute opacity-0" type="checkbox" value={region} bind:group={regions} />
-			<p>{region}</p>
+			<span>
+				<input class="absolute opacity-0" type="checkbox" value={region} bind:group={regions} />
+				<p>{region}</p>
+			</span>
 		</label>
 	{/each}
-</div>
+</fieldset>
