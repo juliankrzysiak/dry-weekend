8a86decfff9ad5f291a426f17c4845bae707a646
Julian Krzysiak <jkrzysiak13@gmail.com>
2024-04-12 17:24:22 -0700

    Revert "Add sentry"
    This reverts commit d03d1cdd489e885c97d7bd52b344219fdfd935d7.

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,7 @@
-import { sentrySvelteKit } from "@sentry/sveltekit";
 import { purgeCss } from 'vite-plugin-tailwind-purgecss';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sentrySvelteKit({
-        sourceMapsUploadOptions: {
-            org: "julian-krzysiak",
-            project: "javascript-sveltekit"
-        }
-    }), sveltekit(), purgeCss()]
+	plugins: [sveltekit(), purgeCss()]
 });
