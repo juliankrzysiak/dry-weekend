fc882dca740f04fe16ea130c2b183c3f272ee3cf
Julian Krzysiak <jkrzysiak13@gmail.com>
2024-04-16 23:42:48 -0700

    Add basic easter egg of snom

---
+++ src/lib/components/EasterEgg.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import { draw, fade, fly, scale } from 'svelte/transition';
+
+	export let clicks = 0;
+	let easterEgg = false;
+</script>
+
+<div class="flex max-w-xl flex-col items-center">
+	{#if clicks >= 3}
+		<!-- <button class="variant-ghost-secondary btn" on:click={playAudio}>Play Easter Egg</button> -->
+		<audio
+			on:play={() => {
+				easterEgg = true;
+			}}
+			on:pause={() => {
+				easterEgg = false;
+			}}
+			controls
+			class="z-10"
+			src="https://dn720302.ca.archive.org/0/items/alice-deejay-better-off-alone/Alice%20Deejay/Better%20Off%20%20Alone/01-Better%20Off%20Alone%20%28Radio%20Edit%29.mp3#t=00:00:57"
+		></audio>
+	{/if}
+
+	{#if easterEgg}
+		<img class="" in:scale out:fade src="/snom-dancing.gif" alt="Snom dancing" />
+	{/if}
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,9 +3,16 @@
 	import EventForm from '$lib/components/EventForm.svelte';
 	import EventTable from '$lib/components/EventTable.svelte';
 	import RegionFilters from '$lib/components/RegionFilters.svelte';
+	import EasterEgg from '$lib/components/EasterEgg.svelte';
 
 	export let data;
 	export let form;
+
+	let clicks = 0;
+
+	function addClick() {
+		clicks += 1;
+	}
 
 	let regions: string[] = [];
 	$: records = regions.length
@@ -13,12 +20,13 @@
 		: data.records;
 </script>
 
-<div class="flex flex-1 flex-col items-center gap-12">
+<div class="flex flex-1 flex-col items-center gap-8">
 	<div class="flex w-full flex-col items-center gap-8">
 		<div class="flex flex-col items-center gap-2">
 			<h2 class="text-xl">upcoming AV and LA events</h2>
-			<h3><i>curated by Julian</i></h3>
+			<h3><i>curated by <button class="italic" on:click={addClick}>Julian</button></i></h3>
 		</div>
+		<EasterEgg bind:clicks />
 		<RegionFilters bind:regions />
 	</div>
 	<EventTable bind:records />
