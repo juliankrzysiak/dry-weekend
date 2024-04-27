9d4efac561b06a18a4b9fa4505e599221aac27dd
Julian Krzysiak <jkrzysiak13@gmail.com>
2024-04-26 22:52:55 -0700

    Add cursor on pseduo-buttons

--- src/lib/components/RegionFilters.svelte
+++ src/lib/components/RegionFilters.svelte
@@ -5,7 +5,7 @@
 <fieldset class="flex w-full max-w-sm justify-center gap-4">
 	{#each ['Antelope Valley', 'Los Angeles'] as region}
 		<label
-			class="variant-ghost-surface btn flex-1 has-[:checked]:variant-ghost-primary has-[:focus-visible]:outline"
+			class="variant-ghost-surface btn flex-1 cursor-pointer has-[:checked]:variant-ghost-primary has-[:focus-visible]:outline"
 		>
 			<span>
 				<input class="absolute opacity-0" type="checkbox" value={region} bind:group={regions} />
