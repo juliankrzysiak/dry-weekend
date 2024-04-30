8f50664afa3662785ba2094b0cd61327ae054ca5
Julian Krzysiak <jkrzysiak13@gmail.com>
2024-04-29 17:14:02 -0700

    Remove importing all components

--- src/lib/index.ts
+++
@@ -1,6 +0,0 @@
-import EventCards from './components/EventCards.svelte';
-import EventForm from './components/EventForm.svelte';
-import EventTable from './components/EventTable.svelte';
-import RegionFilters from './components/RegionFilters.svelte';
-
-export default {* as components}
