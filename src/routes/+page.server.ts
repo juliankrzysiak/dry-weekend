7dcf5fd033754feb4e8b61e0af1cff71c6845e9d
Julian Krzysiak <jkrzysiak13@gmail.com>
2024-05-10 17:19:00 -0700

    Keep event until end of day

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -5,16 +5,17 @@ const pbURL = 'https://dry-weekend.pockethost.io/';
 
 // I can only store the db date in UTC, and I dont wan't to convert it to the "correct time" there.
 // But the local time is taking the time offset into account, so I have to subtract that in order to compare the two.
-function convertToLocalUTC(date: Date): string {
+function parseDate(date: Date): string {
 	const convertedDate = sub(date, { hours: 7 });
-	return convertedDate.toISOString().replace('T', ' ');
+	return convertedDate.toISOString().slice(0, 10);
 }
 
 export async function load({ cookies }) {
 	const client = new PocketBase(pbURL);
-	const now = convertToLocalUTC(new Date(Date.now()));
+	const todayDate = parseDate(new Date(Date.now()));
+	// Comparing timestamp to date to keep today's event visible until tomorrow
 	const records = await client.collection('events').getFullList({
-		filter: `date >= "${now}" || endDate >= "${now}"`,
+		filter: `date >= "${todayDate}" || endDate >= "${todayDate}"`,
 		sort: '-date'
 	});
 
