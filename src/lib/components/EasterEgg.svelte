ebc7ca76d0f88c5355ea8e70f8c0f71817371311
Julian Krzysiak <jkrzysiak13@gmail.com>
2024-06-04 01:13:02 -0700

    Move reset easter egg button down

--- src/lib/components/EasterEgg.svelte
+++ src/lib/components/EasterEgg.svelte
@@ -20,13 +20,13 @@
 
 {#if clicks >= 3}
 	<div class="flex max-w-xl flex-col items-center gap-4">
-		<button on:click={resetEasterEgg} class="variant-ghost-error btn">Reset Easter Egg</button>
 		<audio
 			on:play={showEasterEgg}
 			on:pause={hideEasterEgg}
 			controls
 			src="https://dn720302.ca.archive.org/0/items/alice-deejay-better-off-alone/Alice%20Deejay/Better%20Off%20%20Alone/01-Better%20Off%20Alone%20%28Radio%20Edit%29.mp3#t=00:00:57"
 		></audio>
+		<button on:click={resetEasterEgg} class="variant-ghost-error btn">Reset Easter Egg</button>
 		{#if easterEgg}
 			<img in:scale out:fade src="/snom-dancing.gif" alt="Snom dancing" />
 		{/if}
