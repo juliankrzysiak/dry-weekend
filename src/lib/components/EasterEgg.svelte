012b3d6867cb3e18ffa6935fe99e56455073d697
Julian Krzysiak <jkrzysiak13@gmail.com>
2024-08-02 20:12:43 -0700

    Change starting time for easter egg song

--- src/lib/components/EasterEgg.svelte
+++ src/lib/components/EasterEgg.svelte
@@ -24,7 +24,7 @@
 			on:play={showEasterEgg}
 			on:pause={hideEasterEgg}
 			controls
-			src="https://dn720302.ca.archive.org/0/items/alice-deejay-better-off-alone/Alice%20Deejay/Better%20Off%20%20Alone/01-Better%20Off%20Alone%20%28Radio%20Edit%29.mp3#t=00:00:57"
+			src="https://dn720302.ca.archive.org/0/items/alice-deejay-better-off-alone/Alice%20Deejay/Better%20Off%20%20Alone/01-Better%20Off%20Alone%20%28Radio%20Edit%29.mp3#t=00:01:11"
 		></audio>
 		<button on:click={resetEasterEgg} class="variant-ghost-error btn">Reset Easter Egg</button>
 		{#if easterEgg}
