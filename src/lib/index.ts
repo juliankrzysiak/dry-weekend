00b751538c8f6e1c15be6a9d51d1b980f97822c8
Julian Krzysiak <jkrzysiak13@gmail.com>
2024-08-07 01:56:54 -0700

    Move function

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,4 +1,4 @@
-import { isAfter, endOfWeek, format } from 'date-fns';
+import { isAfter, endOfWeek, format, sub } from 'date-fns';
 import type { RecordModel } from 'pocketbase';
 
 export function formatDate(date: string, endDate: string): string {
@@ -17,4 +17,11 @@ export function checkLastEventOfWeek(records: RecordModel[], index: number) {
 	const endDate = endOfWeek(records[index]?.endDate || records[index]?.date, { weekStartsOn: 2 });
 	const nextDate = records[index + 1]?.endDate || records[index + 1]?.date;
 	return isAfter(nextDate, endDate);
+}
+
+// I can only store the db date in UTC, and I dont wan't to convert it to the "correct time" there.
+// But the local time is taking the time offset into account, so I have to subtract that in order to compare the two.
+export function parseDate(date: Date): string {
+	const convertedDate = sub(date, { hours: 7 });
+	return convertedDate.toISOString().slice(0, 10);
 }
--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,14 +1,8 @@
-import { addMonths, sub } from 'date-fns';
+import { parseDate } from '$lib';
+import { addMonths} from 'date-fns';
 import PocketBase from 'pocketbase';
 
 const pbURL = 'https://dry-weekend.pockethost.io/';
-
-// I can only store the db date in UTC, and I dont wan't to convert it to the "correct time" there.
-// But the local time is taking the time offset into account, so I have to subtract that in order to compare the two.
-function parseDate(date: Date): string {
-	const convertedDate = sub(date, { hours: 7 });
-	return convertedDate.toISOString().slice(0, 10);
-}
 
 export async function load({ cookies }) {
 	const client = new PocketBase(pbURL);
