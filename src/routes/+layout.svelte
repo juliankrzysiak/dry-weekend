55b3f6aa98dd10c003cdf7cfdf802cb02ff8c856
Julian Krzysiak <jkrzysiak13@gmail.com>
2025-03-07 23:49:08 -0800

    Update monthly flair

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,7 +5,7 @@
 
 <div class="flex min-h-screen flex-col px-8 pb-2 pt-8">
 	<header class="flex justify-between pb-8 text-2xl">
-		<h1 title="Love is in the air">dry weekend</h1>
+		<h1 title="Luck is on your side!">dry weekend</h1>
 		<LightSwitch />
 	</header>
 	<main class="flex-1">
@@ -29,7 +29,7 @@
 
 <style>
 	h1::after {
-		content: '❤️';
+		content: '🍀';
 		padding-left: 0.5rem;
 	}
 
